7e09d26a5e3a53486ecacacfb1b548f8af06e377
windfly <974589985@qq.com>
2020-06-11 17:44:31 +0800

    初始化gulp任务

--- code/gulp-web/gulpfile.js
+++ code/gulp-web/gulpfile.js
@@ -1,1 +1,63 @@
 // 实现这个项目的构建任务
+const gulp = require('gulp')
+const del = require('del')
+const plugins = require('gulp-load-plugins')()
+
+// 清空
+const clean = () => {
+  return del(['temp', 'dist'])
+}
+
+// lint
+const lint = () => {
+
+}
+
+// style编译
+const css = () => {
+  return gulp.src('src/assets/styles/**.scss', { base: 'src'})
+    .pipe(plugins.sass())
+    .pipe(gulp.dest('temp'))
+}
+
+// script编译
+const script = () => {
+  gulp.src('src/assets/scripts/**.js', { base: 'src' })
+    .pipe(plugins.babel())
+    .pipe(gulp.dest('temp'))
+}
+
+// html编译
+
+const html = () => {
+
+}
+
+// serve
+const serve = () => {
+
+}
+
+// build
+const build = () => {
+
+}
+
+// start
+const start = () => {
+
+}
+// deploy
+const deploy = () => {
+
+}
+module.exports = {
+  clean,
+  lint,
+  css,
+  script,
+  serve,
+  build,
+  start,
+  deploy
+}
